15030d8ba9bbd4c801fad965e0b127ae1b488dd3
Fabian Damken <fabian.damken@prodyna.com>
2020-05-16 00:04:52 +0200

    Added one LaTeX file (all.tex) to bundle all summaries into one document.

---
+++ summaries/cs/collect.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+
+echo "\\newcommand{\lstbasepath}{unset}"
+for content_tex in $(find mandatory elective -name 'content.tex' | sed 's/mandatory/1-mandatory/' | sed 's/elective/2-elective/' | sort); do
+    content_tex="$(sed -r 's/(1-|2-)//' <<<"$content_tex")"
+    dir="$(dirname "$content_tex")"
+    metadata="$dir/metadata.txt"
+    author="$(head -n 1 "$metadata" | tail -n 1)"
+    title="$(head -n 2 "$metadata" | tail -n 1)"
+    language="$(head -n 3 "$metadata" | tail -n 1)"
+    module="$(basename $(dirname "$content_tex"))"
+    category="$(basename "$(dirname "$(dirname "$content_tex")" )" )"
+    uber_category="$(basename "$(dirname "$(dirname "$(dirname "$content_tex")" )" )" )"
+    content="$(echo "$content_tex" | sed 's/.tex//g')"
+
+    echo "\\part{$title}"
+    echo "\\graphicspath{{./$dir/}}"
+    echo "\\renewcommand{\lstbasepath}{./$dir}"
+    echo "\\include{$content}"
+done

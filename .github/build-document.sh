e6b9452d4e9fabdc87edb9f9bf1c8595badad6c3
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2022-11-06 21:35:37 +0100

    ci: use matrix strategy

---
+++ .github/build-document.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+department="$1"
+type="$2"
+category="$3"
+subject="$4"
+if [[ "$department" == "" ]] || [[ "$type" == "" ]] || [[ "$category" == "" ]] || [[ "$subject" == "" ]]; then
+    echo "E: Invalid arguments. Usage: $0 <department> <type> <category> <subject>" >&2
+    exit 128
+fi
+
+set -o errexit
+set -o nounset
+
+
+# Extract the root repository directory from the location of this script.
+root="$(dirname $(dirname $(readlink -f $0)))"
+
+
+document_id="$department/$type/$category/$subject"
+document_dir="$root/summaries/$document_id"
+document_tex="$document_dir/$subject-summary.tex"
+
+echo "Building $document_id"
+
+echo "Checking if TeX file exists."
+if [[ -f "$document_tex" ]]; then
+    echo "Starting build."
+    cd "$document_dir"
+    echo "Copying fdsummary class and tuda_logo."
+    cp -r "$root/sys/tuda_logo.pdf" "$root/pkg/"* .
+    "$root/bin/compile-summary"
+    cd -
+else
+    echo "W: Cannot find TeX file for $document_id. Skipping." >&2
+fi
---
+++ .github/collect-documents.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+scope="$1"
+if [[ "$scope" != all ]] && [[ "$scope" != changed ]]; then
+    echo "E: Scope '$scope' is neither 'all' nor 'changed'. Usage: $0 <scope:all|changed>" >&2
+    exit 128
+fi
+
+set -o errexit
+set -o nounset
+
+
+if [[ "$scope" == "changed" ]]; then
+    first_commit="4f56a06ecd6c7accc897ce58f9ca458a83de2e3a"  # This will never change.
+    latest_successful_commit="$(curl -s 'https://api.github.com/repos/fdamken/summaries/actions/runs' | jq -r "
+        .workflow_runs
+            | map(select(.status == \"completed\" and .conclusion == \"success\"))
+            | .[0].head_commit.id
+            | if . == null then \"$first_commit\" else . end
+    ")"
+    echo "Collecting documents changed since commit $latest_successful_commit." >&2
+    set +o errexit
+    files="$(git diff --name-only "$latest_successful_commit")"
+    exit_code="$?"
+    set -o errexit
+    if [[ $exit_code -ne 0 ]]; then
+        echo "Collecting changed documents failed; falling back to collecting everything." >&2
+        scope="all"
+    fi
+fi
+if [[ "$scope" == "all" ]]; then
+    echo "Collecting everything." >&2
+    files="$(find . -type f | sed 's@^./@@g')"
+fi
+
+# Extract documents from the found files.
+documents="$(echo "$files" | sed -nr 's@^summaries/([^/]+)/([^/]+)/([^/]+)/([^/]+)/.+tex$@\1 \2 \3 \4@g p' | sort | uniq)"
+
+# And output the JSON array for GitHub to parse.
+echo "$documents" | paste -sd "," | jq -R 'split(",") | .[] |= tostring'
